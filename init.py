f48fb05520909bfed5bb4b8bb2249dcb72d97a20
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-05-11 00:27:17 +0800

    part 2

--- init.py
+++ init.py
@@ -4,13 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '''<h1 class="text-center mx-auto mt-4 text-4xl">Netflix says hi</h1>'''
+    return render_template("home.html")
 
-@app.route("/hello")
+@app.route("/hello/")
 @app.route("/hello/<name>")
 def hello(name=None):
-    return render_template("helloworld.html",name=name.capitalize())
+    return render_template("helloworld.html",name=name)
 
 @app.route("/test")
 def test():
     return render_template("test2.html")
+
+@app.route("/karl")
+def karl():
+    return render_template("index.html")
